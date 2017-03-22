3e43f660e9d94fc424af7de4794f8c3a9041619f
Yancy Knight <yancy.knight@gmail.com>
2017-03-22 10:23:20 -0600

    Add authorization middleware to user router.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', function(req,res,next){
+  if(!req.user){
+    res.redirect('/');
+  }
+  next();
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('users', {user: {
